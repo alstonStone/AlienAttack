5f4454614497e3f26cb100f4fc0b5605092fd24c
alstonStone <alstonStone0@gmail.com>
2024-01-18 17:50:05 -0500

    added player movment controll

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -1,1 +1,16 @@
 extends CharacterBody2D
+
+var speed = 400
+
+func _physics_process(delta):
+	velocity = Vector2(0,0)
+	if Input.is_action_pressed("right"):
+		velocity.x = speed
+	if Input.is_action_pressed("left"):
+		velocity.x = -speed
+	if Input.is_action_pressed("up"):
+		velocity.y = -speed
+	if Input.is_action_pressed("down"):
+		velocity.y = speed
+	move_and_slide()
+	print(velocity)
