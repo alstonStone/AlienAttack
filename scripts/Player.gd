790b5992bfe7a67eb05de71d3be6660418a3ef4a
alstonStone <alstonStone0@gmail.com>
2024-01-19 01:56:08 -0500

    added shooting rockets with space and left mouse button HAS BUGS - with rocket positioning

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -1,8 +1,15 @@
 extends CharacterBody2D
 
 var speed = 400
+var rocket_scene = preload("res://scenes/rocket.tscn")
+
+func _process(delta):
+	#actions
+	if Input.is_action_just_pressed("shoot"):
+		shoot()
 
 func _physics_process(delta):
+	#movement
 	velocity = Vector2(0,0)
 	if Input.is_action_pressed("right"):
 		velocity.x = speed
@@ -12,7 +19,12 @@ func _physics_process(delta):
 		velocity.y = -speed
 	if Input.is_action_pressed("down"):
 		velocity.y = speed
-	
 	move_and_slide()
 	var screen_size = get_viewport_rect().size	
 	global_position = global_position.clamp(Vector2(0,0), screen_size)
+
+
+func shoot():
+	var rocket_instance = rocket_scene.instantiate()
+	add_child(rocket_instance)
+	rocket_instance.global_position.x += 60
