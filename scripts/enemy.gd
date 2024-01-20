2a6b902417e9a5f96a71471d56c2ad65f2e7e6c5
alstonStone <alstonStone0@gmail.com>
2024-01-19 19:36:18 -0500

    added enemy

---
+++ scripts/enemy.gd
@@ -0,0 +1,15 @@
+extends Area2D
+
+
+@export var speed = 200
+@onready var offscreen_notifier = $OffScreenNotifier
+
+func _ready():
+	offscreen_notifier.connect("screen_exited",_on_screen_exited)
+
+func _physics_process(delta):
+	global_position.x -= speed*delta
+
+
+func _on_screen_exited():
+	queue_free()
