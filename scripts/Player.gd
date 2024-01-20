922f8d015cbdb5055f0e9fe4e0667516b7b91027
alstonStone <alstonStone0@gmail.com>
2024-01-19 19:18:12 -0500

    rockets are now uneffected by player moving. this is done by having a node to act as a rocket container. the node doesnt have a transform property so it does not move.

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -2,6 +2,8 @@ extends CharacterBody2D
 
 var speed = 400
 var rocket_scene = preload("res://scenes/rocket.tscn")
+@onready var rocket_container = get_node("RocketContainer")
+
 
 func _process(delta):
 	#actions
@@ -26,5 +28,6 @@ func _physics_process(delta):
 
 func shoot():
 	var rocket_instance = rocket_scene.instantiate()
-	add_child(rocket_instance)
+	rocket_instance.global_position = global_position
+	rocket_container.add_child(rocket_instance)
 	rocket_instance.global_position.x += 60
